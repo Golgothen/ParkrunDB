957c0d87ea8d9b7b10265f37bf749eb175514a88
Paul Ellis <golgothen@gmail.com>
2018-02-02 22:26:55 +1100

    Updated .gitignore

--- Test.py
+++
@@ -1,7 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('--country', nargs = '+', default = 'Australia',  help = 'Specify a country to import by name. Surround the country name with double quotes if it contains a space.')
-args = parser.parse_args()
-print(args)
-for c in args.country:
-    print(c)
