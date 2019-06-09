aa1473f3ebd80d1cf21637876d2f2b92b385ff0c
Paul <golgothen@gmail.com>
2019-06-09 16:00:32 +1000

    Updated run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-python parkrundl.py --mode newevents --processes 3 --delay 10 --region victoria
-python parkrundl.py --mode newevents --processes 3 --delay 10 --country australia --exclude_region victoria
+python parkrundl.py --mode newevents --processes 1 --delay 2 --region victoria
+python parkrundl.py --mode newevents --processes 1 --delay 2 --country australia --exclude_region victoria
