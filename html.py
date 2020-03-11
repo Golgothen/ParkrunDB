33ea20372898ddcf3dae6e48b9efdf55e01eef6b
Paul Ellis <golgothen@gmail.com>
2020-03-12 03:02:54 +1100

    updates

--- html.py
+++ html.py
@@ -16,31 +16,92 @@ config['handlers']['queue']['queue'] = loggingQueue
 
 c = Connection(config)
 
-data = c.execute("select * from getParkrunReportDetail('Wyndham Vale') order by [Total Runs] desc")
+#fstr = lambda s: '' if s is None or s == 'NULL' else str(s)
+parkrun = 'Wyndham Vale'
+data = c.execute(f"select * from getParkrunReportDetail('{parkrun}') order by [Total Runs] desc")
 
 root = e.Element('html', version = '5.0')
 
 body = e.SubElement(root, 'body')
-
+h = e.SubElement(body,'h3')
+h.text = f'Upcomming milestones for {parkrun} parkrun'
 t = e.SubElement(body,'table')
-th = e.SubElement(t,'tr')
+t.attrib['class'] = 'sortable'
+thead = e.SubElement(t,'thead')
+tr = e.SubElement(thead,'tr')
 for i in list(data[0].keys()):
-    td = e.SubElement(th,'td')
+    td = e.SubElement(tr,'th')
     td.text = i
+    td.attrib['class'] = 'header'
 
+rowcount = 0
 for row in data:
-    tr = e.SubElement(t, 'tr')
+    cls = ''
+    if 47 <= row['Total Runs'] <= 49 or \
+       97 <= row['Total Runs'] <= 99 or \
+       247 <= row['Total Runs'] <= 249 or \
+       497 <= row['Total Runs'] <= 499 or \
+       97 <= row['Runs at Home'] <= 99:
+        cls = 'milestone'
+    else:
+        if rowcount == 0:
+            cls = 'altrow1'
+        else:
+            cls = 'altrow2'
+    tr = e.SubElement(t, 'tr', CLASS=cls)
+    rowcount += 1
+    if rowcount> 1:
+        rowcount = 0
+    else:
+        cls = ''
     for k, v in row.items():
         td = e.SubElement(tr, 'td')
         td.text = str(v)
+        if k != 'Barcode':
+            cls = ' notfirstcol' 
+        if row == data[-1] and 'lastrow' not in cls:
+            cls += ' lastrow'
+
+        if len(cls) > 0:
+            td.attrib['class'] = cls
 
 s = e.SubElement(body,'style')
 s.text = """ 
             body {
-                font-family: sans-serif;
+                font-family: 'Montserrat', sans-serif;
+                background-color: Azure
             }
-            table, td, td{
+            table{
+                border-spacing : 0;
+                padding : 20px;
+                cellspacing : 4;
+                cellpadding : 0;
+            }
+            .milestone {
+                background-color: Yellow
+            }
+            .altrow1 {
+                background-color: PaleGreen
+            }
+            .altrow2 {
+                background-color: DarkSeaGreen
+            }
+            .lastrow {
+                border-bottom : 1px solid;
+            }
+            .notfirstcol {
+                border-left : 0;
+            }
+            tr, td {
+                font-size : 14px;
+                vertical-align : middle;
+                padding : 5px;
                 border: 1px solid black;
+                border-bottom : 0;
+            }
+            th {
+                font-size : 16px';
+                font-weight : normal;
             }
         """
 
