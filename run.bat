bc642a7c8a58bdd9263a78c61c1aed0c801fb069
Paul <golgothen@gmail.com>
2021-01-24 20:34:38 +1100

    Included Victorian stats after lockdown

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 python parkruncalendar.py
-rem python parkrundl.py --mode newevents --processes 1 --delay 2 --region victoria  --volunteer yes
+python parkrundl.py --mode newevents --processes 1 --delay 2 --region victoria  --volunteer yes
 python parkrundl.py --mode newevents --processes 1 --delay 2 --country australia --exclude_region victoria --volunteer yes
