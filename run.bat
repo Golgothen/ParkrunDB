5a444fd77bba9d1ba8e7d7ee1c0b4d5e7fe2ffd9
Paul <golgothen@gmail.com>
2021-04-09 12:29:15 +1000

    Added New Zealand to local stats collection

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
 python parkruncalendar.py
-python parkrundl.py --mode newevents --processes 1 --delay 2 --region victoria  --volunteer yes
-python parkrundl.py --mode newevents --processes 1 --delay 2 --country australia --exclude_region victoria --volunteer yes
+python parkrundl.py --mode newevents --processes 1 --delay 10 --region victoria  --volunteer yes
+python parkrundl.py --mode newevents --processes 1 --delay 10 --country australia --exclude_region victoria --volunteer yes
+python parkrundl.py --mode newevents --processes 1 --delay 10 --country "New Zealand" --volunteer yes
